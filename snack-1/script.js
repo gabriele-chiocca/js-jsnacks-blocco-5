07d93f4e7266e797c6808735336a91c3c61719db
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-01-21 15:23:24 +0100

    First snack with forEach

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,7 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+names.forEach((index, element, array) => {
+  console.log(index);
+});
